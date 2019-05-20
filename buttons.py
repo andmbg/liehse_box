9c08e121b732109a29d261c4b112c5f7640089be
Andreas Domberg <andom@posteo.de>
2019-05-20 06:52:54 +0200

    debounce and delay work, getting clean chords.

--- buttons.py
+++ buttons.py
@@ -1,40 +1,41 @@
 import RPi.GPIO as GPIO
 import time
 import collections
+import random
+from threading import Timer
 
-Quintet = collections.namedtuple("Quintet",
-                                 "ts red green white black")
+
+Chord = collections.namedtuple("Chord",
+                               "timestamp black green red white")
 
 class Record_entry():
-    def __init__(self, timestamp, btn_states):
+    def __init__(self, timestamp, black, green, red, white):
         # check arguments:
-        assert type(btn_states) is list and \
-                   len(btn_states) == 4 and \
-                   sum([i in (0, 1) for i in btn_states]) == 4, \
-               "btn_states must be a list (0,1) of length 4."
+        #assert type(btn_states) is list and \
+        #           len(btn_states) == 4 and \
+        #           sum([i in (0, 1) for i in btn_states]) == 4, \
+        #       "btn_states must be a list (0,1) of length 4."
         
         assert type(timestamp) is float, "timestamp must be float."
         
         self.timestamp = timestamp
-        self.red       = btn_states[0]
-        self.green     = btn_states[1]
-        self.white     = btn_states[2]
-        self.black     = btn_states[3]
+        self.red       = red
+        self.green     = green
+        self.white     = white
+        self.black     = black
     
-        self.chord = [self.red, self.green, self.white, self.black]
-        
         self.number_pressed = self.red + \
-                         self.green + \
-                         self.white + \
-                         self.black
+                              self.green + \
+                              self.white + \
+                              self.black
         
     def show(self, names = True):
         if names:
-            return Quintet(ts = self.timestamp,
-                           red = self.red,
-                           green = self.green,
-                           white = self.white,
-                           black = self.black )
+            return Chord(timestamp = self.timestamp,
+                         black = self.black,
+                         green = self.green,
+                         red = self.red,
+                         white = self.white)
         else:
             return (self.timestamp, self.red, self.green, self.white, self.black)
     
@@ -42,7 +43,7 @@ class Record_entry():
 
 class Record():
     def __init__(self):
-        self.entries = [Record_entry(0.0, [0,0,0,0])]
+        self.entries = []
 
     def add_entry(self, entry):
         assert type(entry) is Record_entry, \
@@ -58,3 +59,62 @@ class Record():
 
 
 
+
+class Checklist():
+    def __init__(self):
+        # structure: ID: [timestamp, successful]
+        self.entries = { "BG": [None, False],
+                         "BR": [None, False],
+                         "BW": [None, False],
+                         "GR": [None, False],
+                         "GW": [None, False],
+                         "RW": [None, False] }
+    
+    def add_if_unique(self, rec):
+        assert type(rec) is Record_entry,\
+               "argument to add_if_unique must be Record_entry type."
+        
+        # is this a 2-combo? (no 3- or 4-combo):
+        if rec.number_pressed != 2: return
+        else:
+            if rec.black == 1 and rec.green == 1: combo = "BG"
+            if rec.black == 1 and rec.red == 1: combo = "BR"
+            if rec.black == 1 and rec.white == 1: combo = "BW"
+            if rec.green == 1 and rec.green == 1: combo = "GR"
+            if rec.green == 1 and rec.white == 1: combo = "GW"
+            if rec.red == 1 and rec.white == 1: combo = "RW"
+
+            if self.entries[combo][0] == None:
+                self.entries[combo][0] = rec.timestamp
+
+
+
+
+
+class Inbox():
+    def __init__(self):
+        self.slots = { 'red': 0,
+                       'green': 0,
+                       'white': 0,
+                       'black': 0 }
+                       
+        self.listener = None
+    
+    def schedule_on(self, slot, delay = 0.2):
+        r = random.random()
+        self.slots[slot] = 1
+        self.listener = r
+        # start timer on 
+        Timer(delay, self.send, [r]).start()
+    
+    def schedule_off(self, slot, delay = 0.2):
+        r = random.random()
+        self.slots[slot] = 0
+        self.listener = r
+        # start timer on 
+        Timer(delay, self.send, [r]).start()
+    
+    def send(self, r):
+        if self.listener == r:
+            print(self.slots.values())
+    
--- server.py
+++ server.py
@@ -3,15 +3,16 @@ from flask_socketio import SocketIO, emit
 import RPi.GPIO as GPIO
 import time
 import collections
+import buttons
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-# Mechanical buttons are noisy, often producing >1 voltage edges per press;
-# this helps to filter parasitic button press logs.
-BOUNCETIME = 200 # milliseconds
+# remove mechanical button bounce and parasitic keylogs:
+BOUNCETIME = 0.5 # seconds
+DELAY = 0.2
 
 # How RPi numbers GPIO pins. Consider BOARD as alternative:
 GPIO.setmode(GPIO.BCM)
@@ -22,6 +23,8 @@ red = 14
 green = 15
 white = 18
 black = 25
+
+
 
 GPIO.setup(red,
            GPIO.IN,
@@ -41,108 +44,125 @@ GPIO.setup(black,
 
 
 
-# setup logs:
-red_log = collections.OrderedDict()
-red_log[time.time()] = 0
+start_time = time.time()
+def timestamp(): return time.time() - start_time
 
-green_log = collections.OrderedDict()
-green_log[time.time()] = 0
 
-white_log = collections.OrderedDict()
-white_log[time.time()] = 0
 
-black_log = collections.OrderedDict()
-black_log[time.time()] = 0
+# create the container of our log, the thing that we will save later:
+button_record = buttons.Record()
+
+
+
+# setup single logs for debouncing (these don't end up in the result):
+red_log   = [("red", timestamp(), 0)]
+green_log = [("green", timestamp(), 0)]
+white_log = [("white", timestamp(), 0)]
+black_log = [("black", timestamp(), 0)]
+
+
+
+# setup checklist for 2-chords already pressed:
+checklist = buttons.Checklist()
+
+# inbox filters parasitic keylogs, returns chords
+inbox = buttons.Inbox()
 
 
 
 # return clean single button events. Clean means no bouncing.
 def button_log(channel, log, this_time, this_state):
     # get time & on/off of the last entry:
-    last_time, last_state = list(log.items())[-1]
+    last_time, last_state = log[-1][1:3]
+    button = {14: "red",
+              15: "green",
+              18: "white",
+              25: "black"}[channel]
     res = None
 
     # is this state different? log it:
     if this_state != last_state:
-        res = (this_time, this_state)
+        res = (button, this_time, this_state)
 
     # this state is the same? if the interval is > BOUNCETIME,
     # it's a 1 glued right before a 0 -- log it as 0, likely
     # ignore the actual 0, leading to a clean button release:
     if this_state == last_state and \
-       this_time - last_time > BOUNCETIME/1000:
-            res = (this_time, 1 - this_state)
+       this_time - last_time > BOUNCETIME:
+            res = (button, this_time, 1 - this_state)
     
-    if res == None: return
-    
-    button = {14: "red",
-              15: "green",
-              18: "white",
-              25: "black"}[channel]
-
-    print(button, this_time, this_state, "logged as: ", res)
-    
-    socketio.emit('my_response',
-                  { 'time': res[0],
-                    'state': res[1] })
-
+    #print(res, 1 - GPIO.input(15))
     return(res)
 
 
 
+
+
 def red_callback(channel):
-    fresh_time = time.time()
-    fresh_state = 1 - GPIO.input(channel)
-    this_log_entry = \
-        button_log(channel, red_log, fresh_time, fresh_state)
-    if not this_log_entry == None:
-        red_log[this_log_entry[0]] = this_log_entry[1]
-
+    # returns (button, time, state):
+    res = button_log(channel,
+                     red_log,
+                     timestamp(),
+                     1 - GPIO.input(channel))
+    if not res == None:
+        red_log.append(res)
+        if res[2] == 1: inbox.schedule_on("red", delay = DELAY)
+        if res[2] == 0: inbox.schedule_off("red", delay = DELAY)
+        
 def green_callback(channel):
-    fresh_time = time.time()
-    fresh_state = 1 - GPIO.input(channel)
-    this_log_entry = \
-        button_log(channel, green_log, fresh_time, fresh_state)
-    if not this_log_entry == None:
-        green_log[this_log_entry[0]] = this_log_entry[1]
-
+    # returns (button, time, state):
+    res = button_log(channel,
+                     green_log,
+                     timestamp(),
+                     1 - GPIO.input(channel))
+    
+    if not res == None:
+        green_log.append(res)
+        if res[2] == 1: inbox.schedule_on("green", delay = DELAY)
+        if res[2] == 0: inbox.schedule_off("green", delay = DELAY)
+        
 def white_callback(channel):
-    fresh_time = time.time()
-    fresh_state = 1 - GPIO.input(channel)
-    this_log_entry = \
-        button_log(channel, white_log, fresh_time, fresh_state)
-    if not this_log_entry == None:
-        white_log[this_log_entry[0]] = this_log_entry[1]
-
+    # returns (button, time, state):
+    res = button_log(channel,
+                     white_log,
+                     timestamp(),
+                     1 - GPIO.input(channel))
+    if not res == None:
+        white_log.append(res)
+        if res[2] == 1: inbox.schedule_on("white", delay = DELAY)
+        if res[2] == 0: inbox.schedule_off("white", delay = DELAY)
+        
 def black_callback(channel):
-    fresh_time = time.time()
-    fresh_state = 1 - GPIO.input(channel)
-    this_log_entry = \
-        button_log(channel, black_log, fresh_time, fresh_state)
-    if not this_log_entry == None:
-        black_log[this_log_entry[0]] = this_log_entry[1]
-
+    # returns (button, time, state):
+    res = button_log(channel,
+                     black_log,
+                     timestamp(),
+                     1 - GPIO.input(channel))
+    if not res == None:
+        black_log.append(res)
+        if res[2] == 1: inbox.schedule_on("black", delay = DELAY)
+        if res[2] == 0: inbox.schedule_off("black", delay = DELAY)            
 
 
 # Set up listeners on GPIOs:
 GPIO.add_event_detect(red,
                       GPIO.BOTH,
-                      bouncetime = 1,
+                      bouncetime = 10,
                       callback = red_callback)
 
 GPIO.add_event_detect(green,
                       GPIO.BOTH,
-                      bouncetime = 1,
+                      bouncetime = 10,
                       callback = green_callback)
 
 GPIO.add_event_detect(white,
                       GPIO.BOTH,
-                      bouncetime = 1,
+                      bouncetime = 10,
                       callback = white_callback)
 
 GPIO.add_event_detect(black,
                       GPIO.BOTH,
-                      bouncetime = 1,
+                      bouncetime = 10,
                       callback = black_callback)
 
 
